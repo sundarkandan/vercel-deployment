c8b7ec44af2e03a88491257305abc653dc9b2154
sundarkandan <sundarkandan23506@gmail.com>
2025-03-09 07:10:41 -0700

    Update product.jsx

--- client/src/components/product.jsx
+++ client/src/components/product.jsx
@@ -9,7 +9,7 @@ function Product(){
   const [Data,setData]=useState([])
 
   useEffect(()=>{
-      axios.get("http://localhost:3400/product").then((res)=>{setData(res.data)
+      axios.get("server-gamma-weld-78.vercel.app/product").then((res)=>{setData(res.data)
      
     }).then( console.log("c"+Data))
   },[])
