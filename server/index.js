4985cbdebed884bf14d1c6dcf8cfe20ed33e92dc
sundarkandan <sundarkandan23506@gmail.com>
2025-03-10 01:51:48 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -71,7 +71,7 @@ app.patch('/update',(req,res)=>{
 app.delete('/delete/:id', (req, res) => {
     const fdata = data.filter(item => item.id !== Number(req.params.id));
     data.push(...fdata)
-    fs.writeFile('./products.json', JSON.stringify(fdata, null, 2), () => {
+    fs.writeFile('./products.json', JSON.stringify(data, null, 2), () => {
         res.json(fdata);
     });
 });
