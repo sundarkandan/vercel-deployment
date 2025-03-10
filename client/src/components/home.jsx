dadd7ac699eb9f3c27b151dbb58890dff36bbec6
sundarkandan <sundarkandan23506@gmail.com>
2025-03-10 02:27:24 -0700

    Update home.jsx

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -8,7 +8,7 @@ function Home(){
         <>
         <div className="container-fluid">
             <div className="row d-flex flex-sm-column flex-md-row">
-                <div className="col col-home order-sm-1">
+                <div className="col col-home">
                     <h1 className="display-5" id="home-t1">The <span className="text-danger"><b>World</b></span> is Your Hands.!</h1><br />
                     <p id="para">Connect, Create, Communicate with Our Phones.</p>
                         <Link to="/product"><button id="home-btn" className="btn btn-danger">Explore Our Products</button></Link>
