b4e2474d3bf06bd549355200df099db45859d38c
sundarkandan <sundarkandan23506@gmail.com>
2025-03-10 03:30:18 -0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,13 @@ function App() {
             
           <div className='navbar d-print-none navbar-expand-md'>
             <h1 className='nav-brand ms-2'><img src="https://icon-library.com/images/tech-icon-png/tech-icon-png-13.jpg" width="60px" alt="" /><span className='text-danger'>Tech</span> Shop</h1>
-            <button className='navbar-toggler' data-bs-target="#navs" data-bs-toggle="collapse"><img src="https://icon-library.com/images/menu-png-icon/menu-png-icon-21.jpg" width="50px" alt="" /></button>
+            <button  className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navs"
+          aria-controls="navs"
+          aria-expanded="false"
+          aria-label="Toggle navigation"><img src="https://icon-library.com/images/menu-png-icon/menu-png-icon-21.jpg" width="50px" alt="" /></button>
           <div id='navs' className='collapse navbar-collapse justify-content-end'>
           <ul className="navbar-nav me-3 ms-2">
               <li className='nav-item '><Link to='/' className='nav-link text-dark'>Home</Link></li>
