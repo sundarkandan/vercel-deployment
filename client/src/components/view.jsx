0cc04903327f52b7bd67fb79331ab22c9869683b
sundarkandan <sundarkandan23506@gmail.com>
2025-03-10 02:19:08 -0700

    Update view.jsx

--- client/src/components/view.jsx
+++ client/src/components/view.jsx
@@ -9,7 +9,7 @@ function View(){
     var camara_rear=product.camara_rear
     var [qty,setQty]=useState(0)
     async function handleclick(){
-        await axios.patch('https://server-gwet.onrender.com/view',{product,qty}).then(res=>alert("avaliable only "+res.data+" stocks")).then(window.print())
+        await axios.patch('https://server-gwet.onrender.com/view',{product,qty}).then(res=>alert("avaliable only "+res.data+" stocks"))
     }
     return(
        <>
