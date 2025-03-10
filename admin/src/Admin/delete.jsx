84d2259a2dc18739da0df61fc063a9cd749231e1
sundarkandan <sundarkandan23506@gmail.com>
2025-03-10 00:54:50 -0700

    Update delete.jsx

--- admin/src/Admin/delete.jsx
+++ admin/src/Admin/delete.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 function Delete(){
     const [products,setProducts]=useState([])
  useEffect(()=>{
-        axios.get("https://server-gwet.onrender.com/delete").then(res=>{setProducts(res)
+        axios.get("https://server-gwet.onrender.com/delete").then(res=>{setProducts(res.data)
             console.log(res)
         })
    },[])
