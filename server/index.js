54df92e02750d56b4169bcee7258949248cc49db
sundarkandan <sundarkandan23506@gmail.com>
2025-03-09 21:42:13 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors=require("cors");
 app.use(express.json())
 
 app.use(cors({
-    origin:"https://client-ten-ivory.vercel.app",
+    origin:"https://client-fep0.onrender.com/",
     methods:['POST','GET','PATCH','DELETE'],
     credentials:true
 }))
