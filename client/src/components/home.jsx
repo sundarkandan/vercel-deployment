e525906b2652c9bd761acc85797a8cd763c5b8cf
sundarkandan <sundarkandan23506@gmail.com>
2025-03-10 02:37:14 -0700

    Update home.jsx

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -17,8 +17,8 @@ function Home(){
                         </Routes>
                        
                 </div>
-                <div className="col col-home order-sm-1">
-                    <img src={Main} className="home-imgs d-sm-none" width="400px" alt="" />
+                <div className="col col-home order-md-1">
+                    <img src={Main} className="home-imgs" width="400px" alt="" />
                 </div>
             </div>
         </div>
